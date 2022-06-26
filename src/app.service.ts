672e673fe7cfb753416127c7308b63747b4f4865
Laith Harb <laithharb@Laiths-iMac.local>
2022-06-26 19:37:00 -0400

    found me

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Is it gonna works???????';
+    return 'price match';
   }
 }
